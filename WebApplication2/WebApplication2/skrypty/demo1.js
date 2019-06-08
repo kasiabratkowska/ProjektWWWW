8eb82633afd9c8dcf910be3190c3f9fabf72fc7b
kasiabratkowska <51383464+kasiabratkowska@users.noreply.github.com>
2019-06-08 22:45:01 +0200

    DokonczenieZadania1

---
+++ WebApplication2/WebApplication2/skrypty/demo1.js
@@ -0,0 +1,12 @@
+﻿function modyfikacja() {
+    document.getElementById('aktual').innerHTML = document.lastModified;
+}
+function startClock() {
+    updateClock();
+    setInterval("updateClock()", 1000);
+}
+function updateClock() {
+    node = document.getElementById("clock");
+    now = new Date();
+    node.innerHTML = now.toLocaleString();
+}
